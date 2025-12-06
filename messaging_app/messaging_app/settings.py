eafc13d6e85bbfb39d8065e106ba1867e3622744
Zayd Antony <zaydantony123@gmail.com>
2025-12-06 17:44:44 +0300

    docker compose

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -111,6 +111,22 @@ DATABASES = {
     }
 }
 
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DATABASE'),
+        'USER': os.getenv('MYSQL_USER'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': os.getenv('MYSQL_HOST', 'db'),
+        'PORT': os.getenv('MYSQL_PORT', '3306'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
